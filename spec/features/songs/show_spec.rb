46deb1751c588f17bf015f2e10a884e0d14369a8
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-12-01 20:06:10 -0700

    not sure what i did here, but i dont want to stash it

--- spec/features/songs/show_spec.rb
+++ spec/features/songs/show_spec.rb
@@ -20,7 +20,31 @@ RSpec.describe 'the songs show page' do
     song = artist.songs.create!(title: "I really Like You", length: 208, play_count: 245)
     song_2 = artist.songs.create!(title: "Call Me Maybe", length: 215, play_count: 450)
     visit "/songs/#{song.id}"
-    save_and_open_page
+
     expect(page).to have_content(artist.name)
   end
+
+  it 'displays the songs title, length, and playcount' do
+    artist = Artist.create!(name: 'Carly Rae Jepsen')
+    song = artist.songs.create!(title: "I really Like You", length: 208, play_count: 245)
+    visit "/songs/#{song.id}"
+
+    expect(page).to have_content(song.title)
+    expect(page).to have_content(song.length)
+    expect(page).to have_content(song.play_count)
+  end
+
+  # When I visit a song show page
+  # I see a link back to the songs index page
+  # when I click this link
+  # I am take to the songs index
+
+  it 'displays a link back to the the songs index page' do
+    artist = Artist.create!(name: 'Carly Rae Jepsen')
+    song = artist.songs.create!(title: "I really Like You", length: 208, play_count: 245)
+    visit "/songs/#{song.id}"
+    save_and_open_page
+    expect(page).to have_link("Back to Index", :href=>"/songs")
+  end
+
 end
